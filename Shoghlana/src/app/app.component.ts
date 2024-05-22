0ef79df677893497d15dc9cb5df743918fef6b90
Asmaa20000 <ah4277844@gmail.com>
2024-05-22 18:43:54 +0300

    Register&login

--- Shoghlana/src/app/app.component.ts
+++ Shoghlana/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import {  ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,LoginComponent,RegisterComponent, ReactiveFormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ Shoghlana/src/app/register/register.component.ts
@@ -0,0 +1,24 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+RegisterForm:FormGroup= new FormGroup({
+  name: new FormControl(null, [Validators.required,Validators.minLength(3), Validators.maxLength(20)]),
+  email: new FormControl (null,[Validators.required, Validators.email]),
+  password: new FormControl (null,[Validators.required,Validators.pattern(/^01[0125][0-9]{8}$/)]),
+  rePassword: new FormControl(null,[Validators.required,Validators.pattern(/^[A-Z][a-z][0-9]{3}$/)]),
+  phone: new FormControl(null,[Validators.required, Validators.pattern(/^01[0125][0-9]{8}$/)])
+
+})
+handleRegister(RegisterForm:FormGroup){
+console.log(RegisterForm);
+}
+}
