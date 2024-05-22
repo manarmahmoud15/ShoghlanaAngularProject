33929ddd12390f27a89290ea8b0efdca92199792
Asmaa20000 <ah4277844@gmail.com>
2024-05-23 00:24:58 +0300

    Home

--- Shoghlana/src/app/register/register.component.ts
+++ Shoghlana/src/app/register/register.component.ts
@@ -11,7 +11,7 @@ import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angula
 })
 export class RegisterComponent {
 RegisterForm:FormGroup= new FormGroup({
-  name: new FormControl(null, [Validators.required,Validators.minLength(3), Validators.maxLength(20)]),
+  name: new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
   email: new FormControl (null,[Validators.required, Validators.email]),
   password: new FormControl (null,[Validators.required,Validators.pattern(/^01[0125][0-9]{8}$/)]),
   rePassword: new FormControl(null,[Validators.required,Validators.pattern(/^[A-Z][a-z][0-9]{3}$/)]),
