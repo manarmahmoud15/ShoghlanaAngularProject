d94b75003299657d7fd9a2c715796e421bdf9ddf
manarmahmoud15 <mahmoudmanar774@gmail.com>
2024-05-23 16:19:15 +0300

    last

--- Shoghlana/src/app/app.component.ts
+++ Shoghlana/src/app/app.component.ts
@@ -7,12 +7,13 @@ import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from "./footer/footer.component";
+import { NavbarComponent } from "./navbar/navbar.component";
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, LoginComponent, RegisterComponent, HomeComponent, ReactiveFormsModule, CommonModule, FooterComponent]
+    imports: [RouterOutlet, LoginComponent, RegisterComponent, HomeComponent, ReactiveFormsModule, CommonModule, FooterComponent, NavbarComponent]
 })
 export class AppComponent {
   title = 'Shoghlana';
