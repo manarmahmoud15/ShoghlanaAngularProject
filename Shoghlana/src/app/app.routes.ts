dfcc3848aac9e130e6c62b0dbe57f33d67733a5f
manarmahmoud15 <mahmoudmanar774@gmail.com>
2024-05-24 12:35:18 +0300

    last

--- Shoghlana/src/app/app.routes.ts
+++ Shoghlana/src/app/app.routes.ts
@@ -2,15 +2,7 @@ import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-export const routes: Routes = [
-  //لازم pATH match
-  { path: '', redirectTo:"register", pathMatch:'full'},
-  { path: 'home', component: HomeComponent },
-  { path: 'register', component: RegisterComponent },
-  {path:'signin', component:LoginComponent},
-  { path: 'freelancers', component: FreelancersComponent },
-  { path: '**', component:NotFoundComponent},
-];
+
 import { FreelancersComponent } from './freelancers/freelancers.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
@@ -22,6 +14,8 @@ import { FreelancerPortfolioComponent } from './freelancer-portfolio/freelancer-
 export const routes: Routes = [
     { path: '', redirectTo: 'register', pathMatch: 'full' },
     { path: 'register', component: RegisterComponent },
+    { path: 'home', component: HomeComponent },
+    {path:'signin', component:LoginComponent},
     { path: 'login', component: LoginComponent },
     { path: 'freelancers', component: FreelancersComponent },
     {
