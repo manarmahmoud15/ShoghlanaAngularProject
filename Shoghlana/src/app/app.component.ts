5afd4c380453499ea7c4a34d5600e76504daa9f0
manarmahmoud15 <mahmoudmanar774@gmail.com>
2024-05-24 18:23:12 +0300

    jobcomponent

--- Shoghlana/src/app/app.component.ts
+++ Shoghlana/src/app/app.component.ts
@@ -8,12 +8,13 @@ import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from "./footer/footer.component";
 import { NavbarComponent } from "./navbar/navbar.component";
+import { JobsComponent } from './jobs/jobs.component';
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, LoginComponent, RegisterComponent, HomeComponent, ReactiveFormsModule, CommonModule, FooterComponent, NavbarComponent , RouterLink , RouterLinkActive]
+    imports: [RouterOutlet, LoginComponent, RegisterComponent, HomeComponent, ReactiveFormsModule, CommonModule, FooterComponent, NavbarComponent , RouterLink , RouterLinkActive , JobsComponent]
 })
 export class AppComponent {
   title = 'Shoghlana';
