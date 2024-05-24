294879bb39d66c0b3544a0c967fbdec34c0b13fd
Asmaa20000 <ah4277844@gmail.com>
2024-05-24 20:44:47 +0300

    edit

--- Shoghlana/src/app/app.routes.ts
+++ Shoghlana/src/app/app.routes.ts
@@ -1,28 +1,29 @@
-import { HomeComponent } from './home/home.component';
-import { NotFoundComponent } from './not-found/not-found.component';
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-export const routes: Routes = [
-  //لازم pATH match
-  { path: '', redirectTo:"register", pathMatch:'full'},
-  { path: 'home', component: HomeComponent },
-  { path: 'register', component: RegisterComponent },
-  {path:'signin', component:LoginComponent},
-  { path: 'freelancers', component: FreelancersComponent },
-  { path: '**', component:NotFoundComponent},
-];
+// export const routes: Routes = [
+//   //لازم pATH match
+//   { path: '', redirectTo:"register", pathMatch:'full'},
+//   { path: 'home', component: HomeComponent },
+//   { path: 'register', component: RegisterComponent },
+//   {path:'signin', component:LoginComponent},
+//   { path: 'freelancers', component: FreelancersComponent },
+//   { path: '**', component:NotFoundComponent},
+// ];
 import { FreelancersComponent } from './freelancers/freelancers.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
-import { NotfoundComponent } from './notfound/notfound.component';
 import { FreelancerProfileComponent } from './freelancer-profile/freelancer-profile.component';
 import { FreelancerWorkHistoryComponent } from './freelancer-work-history/freelancer-work-history.component';
 import { FreelancerPortfolioComponent } from './freelancer-portfolio/freelancer-portfolio.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'register', pathMatch: 'full' },
     { path: 'register', component: RegisterComponent },
-    { path: 'login', component: LoginComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'signin', component: LoginComponent },
     { path: 'freelancers', component: FreelancersComponent },
     {
         path: 'freelancerprofile/:id', component: FreelancerProfileComponent, children: [
@@ -31,7 +32,7 @@ export const routes: Routes = [
             { path: 'workhistory', component: FreelancerWorkHistoryComponent }
         ]
     },
-    { path: '**', component: NotfoundComponent }
+    { path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
