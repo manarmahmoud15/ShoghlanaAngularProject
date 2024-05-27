863c27cd3147962aa70c3a93d953383ad3533d0b
Omar-Abo-Ziada <164497850+Omar-Abo-Ziada@users.noreply.github.com>
2024-05-27 17:11:15 +0300

    fixed some issue in  app.routes

--- Shoghlana/src/app/app.routes.ts
+++ Shoghlana/src/app/app.routes.ts
@@ -1,16 +1,6 @@
 import { HomeComponent } from './home/home.component';
-import { NotFoundComponent } from './not-found/not-found.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-export const routes: Routes = [
-  //لازم pATH match
-  { path: '', redirectTo:"register", pathMatch:'full'},
-  { path: 'home', component: HomeComponent },
-  { path: 'register', component: RegisterComponent },
-  {path:'signin', component:LoginComponent},
-  { path: 'freelancers', component: FreelancersComponent },
-  { path: '**', component:NotFoundComponent},
-];
 import { FreelancersComponent } from './freelancers/freelancers.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
@@ -23,6 +13,7 @@ export const routes: Routes = [
     { path: '', redirectTo: 'register', pathMatch: 'full' },
     { path: 'register', component: RegisterComponent },
     { path: 'login', component: LoginComponent },
+    { path: 'homw', component: HomeComponent },
     { path: 'freelancers', component: FreelancersComponent },
     {
         path: 'freelancerprofile/:id', component: FreelancerProfileComponent, children: [
