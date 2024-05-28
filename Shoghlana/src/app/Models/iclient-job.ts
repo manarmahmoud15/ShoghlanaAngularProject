f02963c9a2897a54dc347556b20c87e122159a28
manarmahmoud15 <mahmoudmanar774@gmail.com>
2024-05-28 17:03:44 +0300

    last

---
+++ Shoghlana/src/app/Models/iclient-job.ts
@@ -0,0 +1,10 @@
+export interface IClientJob {
+    id:Number ,
+    title : String ,
+    MinPrice : string,
+    MaxPrice : string ,
+    status : string, 
+    clientName : string ,
+    clientImg : string ,
+    catID : Number 
+}
--- Shoghlana/src/app/projects/projects.component.ts
+++ Shoghlana/src/app/projects/projects.component.ts
@@ -5,6 +5,7 @@ import { ICategory } from '../Models/icategory';
 import { RouterLink } from '@angular/router';
 import { HighlightDirective } from '../directives/highlight.directive';
 import { HttpClientModule } from '@angular/common/http';
+import { IClientJob } from '../Models/iclient-job';
 
 @Component({
   selector: 'app-projects',
@@ -16,7 +17,7 @@ import { HttpClientModule } from '@angular/common/http';
 export class ProjectsComponent {
   category: ICategory[];
   selectedCategoryID: number = 0;
-
+  ClientJob : IClientJob[] ;
   constructor() {
     this.category = [
       { id: 1, name: 'أعمال وخدمات ادارية واستشارية' },
@@ -25,5 +26,38 @@ export class ProjectsComponent {
       { id: 4, name: 'فيديو' },
       { id: 5, name: 'كتابة و ترجمة ولغات' }
     ];
+    this.ClientJob = [
+      {
+        id: 1,
+        title: "مطلوب متخصص لعمل اسكربت علي موقع حجز مواعيد تأشيرة",
+        MinPrice: '25$',
+        MaxPrice: '50$',
+        status: 'مفتوح',
+        clientName: 'منار',
+        clientImg: "../../assets/imgs/Nerd-amico.png",
+        catID: 1
+      },
+      {
+        id: 2,
+        title: " تعديلات و إعادة تصميم موقع ووردبريس",
+        MinPrice: '10$',
+        MaxPrice: '30$',
+        status: 'مغلق',
+        clientName: 'أحمد',
+        clientImg: "../../assets/imgs/Nerd-amico.png",
+        catID: 2
+      },
+      {
+        id: 3,
+        title: "موقع ومتجر إلكتروني لجمعية خيرية",
+        MinPrice: '20$',
+        MaxPrice: '40$',
+        status: 'مفتوح',
+        clientName: 'سارة',
+        clientImg: "../../assets/imgs/Nerd-amico.png",
+        catID: 3
+      },
+    ];
+    
   }
 }
