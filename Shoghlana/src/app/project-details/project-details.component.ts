e1a913aa61806270db6861ba1de6bc4264a28b3b
manarmahmoud15 <mahmoudmanar774@gmail.com>
2024-06-02 14:17:50 +0300

    last

---
+++ Shoghlana/src/app/project-details/project-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-project-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './project-details.component.html',
+  styleUrl: './project-details.component.css'
+})
+export class ProjectDetailsComponent {
+
+}
