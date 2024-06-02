f5de55ff5710765cef0a72ac862c0d434706fead
Asmaa20000 <ah4277844@gmail.com>
2024-06-02 20:15:18 +0300

    Login &Register Done

--- Shoghlana/src/app/register/register.component.ts
+++ Shoghlana/src/app/register/register.component.ts
@@ -1,31 +1,63 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Observable } from 'rxjs';
 import { AuthService } from '../auth.service';
+import { error } from 'console';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule, HttpClientModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
-
 export class RegisterComponent {
-RegisterForm:FormGroup
-constructor(){
-  this.RegisterForm= new FormGroup({
-    name: new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(20)]),
-    email: new FormControl (null,[Validators.required, Validators.email]),
-    password: new FormControl (null,[Validators.required,  Validators.pattern(/^(?=.*\d{3,})[A-Za-z\d]{5,}$/)]),
-    rePassword: new FormControl(null,[Validators.required,  Validators.pattern(/^(?=.*\d{3,})[A-Za-z\d]{5,}$/)]),
-    phone: new FormControl(null,[Validators.required, Validators.pattern(/^01[0125][0-9]{8}$/)])
-
-  })
-}
+  isLoading:boolean=false;
+  apiError:string=''
+  RegisterForm: FormGroup;
+  constructor(private _authoService: AuthService, private _router: Router) {
+    this.RegisterForm = new FormGroup({
+      username: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(20),
+      ]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required]),
+      repeatPassword: new FormControl(null, [Validators.required]),
+      phoneNumber: new FormControl(null, [
+        Validators.required,
+        Validators.pattern(/^01[0125][0-9]{8}$/),
+      ]),
+    });
+  }
   handleRegister(RegisterForm: FormGroup) {
-    console.log(this.RegisterForm.value);
+    this.isLoading=true;
+    if (RegisterForm.valid) {
+      console.log(RegisterForm.value);
+      this._authoService.register(RegisterForm.value).subscribe({
+        next: (response) => {
+          console.log(response);
+          if (response.isSuccess) {
+            this.isLoading=false
+            this._router.navigateByUrl("/signin")
+          }
+        },
+        error: (error) => {
+          this.isLoading=false
+          this.apiError=error.error.errors
+          alert(this.apiError)
+
+        }
+      });
+    }
   }
 }
