e92a429084a13a959859f370698295a62d129640
manarmahmoud15 <mahmoudmanar774@gmail.com>
2024-06-20 18:06:47 +0300

    last

--- Shoghlana/src/app/app.config.ts
+++ Shoghlana/src/app/app.config.ts
@@ -5,9 +5,17 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withFetch, withInterceptors,HttpClientModule } from '@angular/common/http';
 import { authinterceptors } from './Interceptors/auth-interceptor.service';
 import { GoogleLoginProvider, SocialAuthServiceConfig } from '@abacritt/angularx-social-login';
- 
+import { provideStore } from '@ngrx/store';
+import { increaseCounter } from './Store/Counter/counter.action';
+import { counterReducer } from './Store/Counter/counter.reducer';
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch(), withInterceptors([authinterceptors]))
+  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch(),
+    withInterceptors([authinterceptors]))
+    ,provideStore({
+counter:counterReducer
+    })
+  //ًwith fetch  عشان اشتغل ب fetch api NOT With httprequest
+  //with interceptors  to use any interceptor in app when requesting data or response
 , {
   provide: 'SocialAuthServiceConfig',
   useValue: {
@@ -31,17 +39,5 @@ export const appConfig: ApplicationConfig = {
     }
   } as SocialAuthServiceConfig,
 }]
-import { provideStore } from '@ngrx/store';
-import { increaseCounter } from './Store/Counter/counter.action';
-import { counterReducer } from './Store/Counter/counter.reducer';
 
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideHttpClient(withFetch(),
-    withInterceptors([authinterceptors]))
-    ,provideStore({
-counter:counterReducer
-    })]
-  //ًwith fetch  عشان اشتغل ب fetch api NOT With httprequest
-  //with interceptors  to use any interceptor in app when requesting data or response
-  
-};
+}
