0ae0eceadb3f4f561e31dab6470eddb84ae857ce
manarmahmoud15 <mahmoudmanar774@gmail.com>
2024-06-21 16:49:11 +0300

    last

--- Shoghlana/src/app/addproject/addproject.component.ts
+++ Shoghlana/src/app/addproject/addproject.component.ts
@@ -1,10 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-addproject',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink ,CommonModule, RouterLink , FormsModule ],
   templateUrl: './addproject.component.html',
   styleUrl: './addproject.component.css'
 })
