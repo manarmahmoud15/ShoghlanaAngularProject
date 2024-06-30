f77f749cb9075435303d3167f10257b92f3f1d95
manarmahmoud15 <mahmoudmanar774@gmail.com>
2024-06-30 15:48:46 +0300

    update

---
+++ Shoghlana/src/app/Services/Notification/notification.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+
+  constructor(private _HttpClient: HttpClient) { }
+  headers:any = {
+    token:localStorage.getItem('token')
+  }
+  AddNotification(ClientID : string):Observable<any>{
+    return this._HttpClient.post(`https://route-ecommerce.onrender.com/api/v1/cart` ,{
+      productId:ClientID
+    },{
+      headers : this.headers 
+    })
+
+  }
+}
