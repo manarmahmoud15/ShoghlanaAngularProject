70b7d50273d12180ec8b3f81cbb0ebbba3e3dee1
manarmahmoud15 <mahmoudmanar774@gmail.com>
2024-07-04 12:14:49 +0300

    update package

--- Shoghlana/src/app/private-chat/private-chat.component.ts
+++ Shoghlana/src/app/private-chat/private-chat.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-private-chat',
   standalone: true,
   imports: [],
   templateUrl: './private-chat.component.html',
-  styleUrl: './private-chat.component.css'
+  styleUrls: ['./private-chat.component.css']
 })
-export class PrivateChatComponent {
+export class PrivateChatComponent implements OnInit, OnDestroy {
+  @Input() toUser = '';
 
+  constructor(private activeModal: NgbActiveModal) {}
+
+  ngOnInit(): void {
+    // Initialization logic here
+    console.log('PrivateChatComponent initialized with toUser:', this.toUser);
+  }
+
+  ngOnDestroy(): void {
+    // Cleanup logic here
+    console.log('PrivateChatComponent destroyed');
+  }
+
+  closeModal() {
+    this.activeModal.close();
+  }
 }
